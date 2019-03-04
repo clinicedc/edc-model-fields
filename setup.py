62869f1b54b43478de230956664b69793385f200
erikvw <ew2789@gmail.com>
2019-03-04 17:31:33 -0600

    update imports for changes in edc-base

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ setup(
     long_description=README,
     zip_safe=False,
     keywords='django edc model field class',
+    install_requires=["edc_constants"],
     classifiers=[
         'Environment :: Web Environment',
         'Framework :: Django',
