317cc74c9aab91656944dcafcd207135d2ad8c56
erikvw <ew2789@gmail.com>
2022-09-25 14:31:47 -0500

    docstrings

--- edc_model_fields/apps.py
+++ edc_model_fields/apps.py
@@ -3,3 +3,4 @@ from django.apps import AppConfig as DjangoAppConfig
 
 class AppConfig(DjangoAppConfig):
     name = "edc_model_fields"
+    verbose_name = "EDC Model Fields"
--- edc_model_fields/models.py
+++
@@ -1,4 +0,0 @@
-from django.conf import settings
-
-if settings.APP_NAME == "edc_model_fields":
-    from .tests.models import TestModel  # noqa
