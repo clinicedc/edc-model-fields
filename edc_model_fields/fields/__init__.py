98769306b38540387fa2e8c381d0120ab6ecde30
erikvw <ew2789@gmail.com>
2024-02-09 14:08:11 -0600

    add custom Choices class, custom field classes with extra attrs

--- edc_model_fields/fields/__init__.py
+++ edc_model_fields/fields/__init__.py
@@ -1,9 +1,7 @@
-from django import get_version
-
+from .custom_django_fields import CharField2, IntegerField2, ManyToManyField2
 from .date_estimated import IsDateEstimatedField, IsDateEstimatedFieldNa
 from .hostname_modification_field import HostnameModificationField
 from .identity_type_field import IdentityTypeField
 from .initials_field import InitialsField
 from .other_charfield import OtherCharField
 from .userfield import UserField
-from .uuid_auto_field import UUIDAutoField
